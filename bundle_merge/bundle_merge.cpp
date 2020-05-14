59368abc373d3decfdd5d498ccbdc58dcda7d677
Eberty <eberty.silva@hotmail.com>
2020-05-13 23:46:01 -0300

    Add texturization of bottom view pipeline

---
+++ bundle_merge/bundle_merge.cpp
@@ -0,0 +1,201 @@
+/*
+ * Copyright (c) 2020, Eberty Alves
+ */
+
+// C++ standard library
+#include <bits/stdc++.h>
+
+// Boost
+#include <boost/program_options.hpp>
+
+struct Cameras {
+  float f, q[9], c[3], d[2];
+};
+
+bool bundlerTxt(const std::string& input_file_path, const std::string& output_file_path,
+                const std::vector<std::string>& images_file_name) {
+  std::ifstream input_file(input_file_path);
+  std::ofstream output_file(output_file_path);
+  if (input_file.is_open() && output_file.is_open()) {
+    std::string token;
+
+    while (std::getline(input_file, token)) {
+      output_file << token << '\n';
+    }
+
+    for (size_t i = 0; i < images_file_name.size(); i++) {
+      output_file << images_file_name[i] << '\n';
+    }
+
+    input_file.close();
+    output_file.close();
+    return true;
+  } else {
+    std::cout << "Unable to open " << input_file_path << " or " << output_file_path << std::endl;
+    return false;
+  }
+}
+
+bool bundlerOut(const std::string& meshlab_file_path, const std::string& input_file_path,
+                const std::string& output_file_path, std::vector<Cameras>& cameras) {
+  std::ofstream output_file(output_file_path);
+  if (!output_file.is_open()) {
+    std::cout << "Unable to open file: " << output_file_path << std::endl;
+    return false;
+  }
+
+  {
+    std::ifstream input_file(meshlab_file_path);
+    if (input_file.is_open()) {
+      std::string token;
+      while (input_file.peek() == '#') {
+        std::getline(input_file, token);
+      }
+
+      int ncam = 0, npoint = 0;
+      input_file >> ncam >> npoint;
+
+      cameras.resize(ncam);
+      for (int i = 0; i < ncam; i++) {
+        input_file >> cameras[i].f >> cameras[i].d[0] >> cameras[i].d[1];
+        for (int j = 0; j < 9; j++) {
+          input_file >> cameras[i].q[j];
+        }
+        input_file >> cameras[i].c[0] >> cameras[i].c[1] >> cameras[i].c[2];
+      }
+
+      input_file.close();
+    } else {
+      std::cout << "Unable to open file: " << meshlab_file_path << std::endl;
+      return false;
+    }
+  }
+
+  {
+    std::ifstream input_file(input_file_path);
+    if (input_file.is_open()) {
+      std::string token;
+      while (input_file.peek() == '#') {
+        std::getline(input_file, token);
+        output_file << token << '\n';
+      }
+
+      int ncam = 0, npoint = 0;
+      input_file >> ncam >> npoint;
+      input_file.ignore();
+
+      output_file << ncam + cameras.size() << " " << npoint << '\n';
+      for (int i = 0; i < ncam; i++) {
+        for (int j = 0; j < 5; j++) {
+          std::getline(input_file, token);
+          output_file << token << '\n';
+        }
+      }
+
+      for (size_t i = 0; i < cameras.size(); i++) {
+        output_file << cameras[i].f << ' ' << cameras[i].d[0] << ' ' << cameras[i].d[1] << '\n';
+        for (int j = 0; j < 9; j++) {
+          output_file << cameras[i].q[j] << (((j % 3) == 2) ? '\n' : ' ');
+        }
+        output_file << cameras[i].c[0] << ' ' << cameras[i].c[1] << ' ' << cameras[i].c[2] << '\n';
+      }
+
+      while (std::getline(input_file, token)) {
+        output_file << token << '\n';
+      }
+
+      input_file.close();
+    } else {
+      std::cout << "Unable to open file: " << input_file_path << std::endl;
+      return false;
+    }
+  }
+
+  output_file.close();
+  return true;
+}
+
+int compare(std::string s1, std::string s2) {
+  std::transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+  std::transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+  return (s1.compare(s2) == 0);
+}
+
+int main(int argc, char* argv[]) {
+  try {
+    // Declaration of variables
+    std::string meshlab_bundle;
+    std::vector<std::string> images;
+
+    std::string bundle_file_name;
+    std::string list_file_name;
+
+    std::string output_prefix;
+
+    // Parse command-line options
+    namespace po = boost::program_options;
+
+    // Define command-line options
+    po::options_description desc("Options");
+    desc.add_options()
+    ("help,h", "Print help message")
+    ("meshlab_bundle,m", po::value<std::string>(&meshlab_bundle)->required(), "Input: meshlab bundle file (.out)")
+    ("images,i", po::value<std::vector<std::string>>(&images)->multitoken(), "Input: vector with images used on meshlab bundle file")
+    ("bundle,b", po::value<std::string>(&bundle_file_name)->required(), "Original bundle file (.out)")
+    ("list,l", po::value<std::string>(&list_file_name)->required(), "Original list file (.txt)")
+    ("prefix,p", po::value<std::string>(&output_prefix)->default_value("merged"), "Prefix for output files");
+
+
+    // Use a parser to evaluate the command line
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+
+    // Store the command-line options evaluated by the parser
+    if (vm.count("help")) {
+      std::cout << "Merge bundle files" << std::endl << std::endl;
+      std::cout << desc << std::endl;
+      return 0;
+    }
+
+    if (vm.count("meshlab_bundle") && vm.count("images") && vm.count("bundle") && vm.count("list")) {
+      meshlab_bundle = vm["meshlab_bundle"].as<std::string>();
+      images = vm["images"].as<std::vector<std::string>>();
+      bundle_file_name = vm["bundle"].as<std::string>();
+      list_file_name = vm["list"].as<std::string>();
+      output_prefix = vm["prefix"].as<std::string>();
+    } else {
+      throw std::string("Correct mode of use: " + std::string(argv[0]) +
+                        " -i <vector_images.png> -m meshlab_bundle.out -b bundle.out -l bundle-list.txt");
+    }
+
+    if (!compare(meshlab_bundle.substr(meshlab_bundle.find_last_of(".") + 1), "out") ||
+        !compare(bundle_file_name.substr(bundle_file_name.find_last_of(".") + 1), "out") ||
+        !compare(list_file_name.substr(list_file_name.find_last_of(".") + 1), "txt")) {
+      throw std::string("Extension on input files are incorrect");
+    }
+
+    std::vector<Cameras> new_cameras;
+    std::string output_file_path =
+        output_prefix + "." + bundle_file_name.substr(0, bundle_file_name.find_last_of(".")) + ".out";
+    if (!bundlerOut(meshlab_bundle, bundle_file_name, output_file_path, new_cameras)) {
+      throw std::string("Error while writing bundler *.out file");
+    }
+
+    if (new_cameras.size() != images.size()) {
+      throw std::string("Image list must have the same number of cameras of " + meshlab_bundle);
+    }
+
+    output_file_path = output_prefix + "." + list_file_name.substr(0, list_file_name.find_last_of(".")) + ".txt";
+    if (!bundlerTxt(list_file_name, output_file_path, images)) {
+      throw std::string("Error while writing bundler *.txt file");
+    }
+
+    return 0;
+  } catch (boost::program_options::error& msg) {
+    std::cout << "ERROR: " << msg.what() << std::endl;
+  } catch (std::string msg) {
+    std::cout << msg << std::endl;
+  }
+
+  return -1;
+}
---
+++ scripts/bottom_view.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+# Copy this script into a folder with images/sfm subfolder
+
+# ----------------------------------------------------------------------
+
+# Set sfm directory
+CUR_DIR=${PWD}
+SFM_DIR=${CUR_DIR}/images/sfm
+
+# Verify directory existence
+if [[ ! -d ${SFM_DIR} || ! "$(ls -A ${SFM_DIR})" ]]; then
+    echo "Directory ${SFM_DIR} not found or empty."
+    return;
+fi
+
+# ----------------------------------------------------------------------
+
+# Verify if the first argument is a valid name
+if [[ ! -f ${1} || ${1: -4} != ".out" ]]; then
+    echo "Please inform a valid '.out' file as argument."
+    return;
+fi
+
+# Verify if the others arguments are valid images
+if [[ ! "${@:2}" ]]; then
+    echo "Please inform valid images."
+    return;
+else
+    for IMG in "${@:2}"; do
+        if [[ ! -f ${IMG} || ! ( ${IMG: -4} == ".png" || ${IMG: -4} == ".jpg" ) ]]; then
+            echo "Please inform a valid images as argument (error on parsing ${IMG})"
+            return;
+        fi
+    done
+fi
+
+# ----------------------------------------------------------------------
+
+# Set mesh file name
+FILE_NAME=${1}
+
+# Set colmap command
+COLMAP_BIN=colmap
+
+# Set openMVS directory
+OPENMVS_DIR=/usr/local/bin/OpenMVS
+
+# Set directory where are the necessary executable for this pipeline
+EXE_DIR=/usr/local/LowCost3DReconstruction
+
+# Set meshlabserver command
+MESHLABSERVER="LC_ALL=C meshlab.meshlabserver"
+
+# ----------------------------------------------------------------------
+
+# Working directory
+cp ${FILE_NAME} ${SFM_DIR}/${FILE_NAME}
+for IMG in "${@:2}"; do
+    cp ${IMG} ${SFM_DIR}/
+done
+cd ${SFM_DIR}
+
+# ----------------------------------------------------------------------
+
+# Convert colmap project into bundler files
+${COLMAP_BIN} model_converter --input_path ${SFM_DIR}/sparse/0 --output_path ${SFM_DIR}/bundler --output_type Bundler
+mv ${SFM_DIR}/bundler.bundle.out ${SFM_DIR}/bundle.out
+mv ${SFM_DIR}/bundler.list.txt ${SFM_DIR}/bundle-list.txt
+
+COUNTER=0
+while IFS= read -r line
+do
+  sed -i "$[$COUNTER +1]s|.*|${SFM_DIR}/undistorted_images/$(printf '%05d' $COUNTER).png|" ${SFM_DIR}/bundle-list.txt
+  COUNTER=$[$COUNTER +1]
+done < "${SFM_DIR}/bundle-list.txt"
+
+${OPENMVS_DIR}/InterfaceVisualSFM ${SFM_DIR}/bundler.out
+
+# ----------------------------------------------------------------------
+
+# Merge bundle files
+${EXE_DIR}/bundle_merge -i ${@:2} -m ${FILE_NAME} -b bundle.out -l bundle-list.txt -p merged
+rm ${SFM_DIR}/bundle.out
+rm ${SFM_DIR}/bundle-list.txt
+
+# ----------------------------------------------------------------------
+
+# Use the new file for texturization
+${OPENMVS_DIR}/InterfaceVisualSFM ${SFM_DIR}/merged.bundle.out --output-file ${SFM_DIR}/final.mvs
+${OPENMVS_DIR}/ReconstructMesh ${SFM_DIR}/final.mvs --mesh-file ${SFM_DIR}/mesh_file.ply --smooth 0 --working-folder ${SFM_DIR}
+
+# ----------------------------------------------------------------------
+
+# Mesh texturing for computing a sharp and accurate texture to color the mesh
+${OPENMVS_DIR}/TextureMesh ${SFM_DIR}/final_mesh.mvs --patch-packing-heuristic 0 --cost-smoothness-ratio 1 --empty-color 16744231 --working-folder ${SFM_DIR}
+
+rm ${SFM_DIR}/*.log
+
+# ----------------------------------------------------------------------
+
+# Copy result to current dir
+cp ${SFM_DIR}/final_mesh_texture.png ${CUR_DIR}/final_mesh_texture.png
+cp ${SFM_DIR}/final_mesh_texture.ply ${CUR_DIR}/final_mesh_texture.ply
+
+cd ${CUR_DIR}
+
+# Convert to OBJ
+eval ${MESHLABSERVER} -i final_mesh_texture.ply -o final_mesh_texture.obj -m vn wt 2> /dev/null
+mv final_mesh_texture.obj.mtl final_mesh_texture.mtl
+sed -i 's/final_mesh_texture.obj.mtl/final_mesh_texture.mtl/' final_mesh_texture.obj
+
+# ----------------------------------------------------------------------
+
+# LC_ALL=C meshlab ${SFM_DIR}/final_mesh_texture.ply &> /dev/null
