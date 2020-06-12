5436158e8d73fa3b79c0a46bed85a6334aa1eef4
Eberty <eberty.silva@hotmail.com>
2020-06-12 15:45:12 -0300

    Add Libfreenect package install

--- install/tools_install.sh
+++ install/tools_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CUR_DIR=${PWD}
+
 # Tools
 sudo apt update
 sudo apt install \
@@ -24,8 +26,21 @@ sudo ln -s /usr/lib/x86_64-linux-gnu/libglut.so /usr/lib/libglut.so
 sudo ln -s /usr/lib/x86_64-linux-gnu/libglut.so.3 /usr/lib/libglut.so.3
 sudo ln -s /usr/lib/x86_64-linux-gnu/libglut.so.3.9.0 /usr/lib/libglut.so.3.9.0
 
-CUR_DIR=${PWD}
+# Compile and install Libfreenect
+sudo apt-get install git cmake build-essential libusb-1.0-0-dev
+sudo apt-get install freeglut3-dev libxmu-dev libxi-dev # only if you are building the examples
+git clone https://github.com/OpenKinect/libfreenect
 
+cd libfreenect
+sudo cp ./platform/linux/udev/51-kinect.rules /etc/udev/rules.d/
+mkdir build
+cd build
+cmake -L .. # -L lists all the project options
+make -j$(nproc) && sudo make install
+
+cd ${CUR_DIR}
+
+# Compile and install Meshlab
 cd ~
 git clone --recursive https://github.com/cnr-isti-vclab/meshlab
 bash meshlab/install/linux/linux_setup_env_ubuntu.sh
