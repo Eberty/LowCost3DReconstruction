904d19d7f7652b14795e7c2faf4a38a6d2b7fe47
Eberty <eberty.silva@hotmail.com>
2020-09-01 18:40:25 -0300

    Add rotate align class with GUI

---
+++ rotate_align/main.cpp
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2019, Eberty Alves
+ */
+
+// Qt library
+#include <QApplication>
+
+// Project files
+#include "rotate_align.h"
+
+int main(int argc, char *argv[]) {
+  QApplication app(argc, argv);
+
+  RotateAlign viewer(argc, argv);
+  if (viewer.userInterface()) {
+    viewer.show();
+    return app.exec();
+  }
+
+  return 0;
+}
